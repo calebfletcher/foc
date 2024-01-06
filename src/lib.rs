75114bd5ccbd367bd8c55e45be7addb1e68d5292
Caleb Fletcher <caleb@fletcher.cf>
2024-01-06 14:11:47 +1100

    feat: add clarke transform

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,20 @@
 #![no_std]
 #![forbid(unsafe_code)]
 
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+use core::f32::consts::FRAC_1_SQRT_2;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+use nalgebra::Matrix3;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+const SQRT_FRAC_2_3: f32 = 0.81649658; // sqrt(2/3)
+const SQRT_3_FRAC_2: f32 = 0.8660254; // sqrt(3)/2
+
+pub fn clarke() -> Matrix3<f32> {
+    #[rustfmt::skip]
+    let mat = Matrix3::new(
+        1., -0.5, -0.5,
+        0., SQRT_3_FRAC_2, 0.,
+        FRAC_1_SQRT_2, FRAC_1_SQRT_2, FRAC_1_SQRT_2
+    );
+
+    SQRT_FRAC_2_3 * mat
 }
