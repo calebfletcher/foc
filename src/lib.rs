e1b15c655c01e5146262419be37caa51053f361e
Caleb Fletcher <caleb@fletcher.cf>
2024-01-06 14:31:15 +1100

    feat: use fixed crate for math

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,31 @@
 #![no_std]
 #![forbid(unsafe_code)]
 
-use core::f32::consts::FRAC_1_SQRT_2;
-
 use nalgebra::Matrix3;
+use simba::scalar::FixedI16F16;
 
-const SQRT_FRAC_2_3: f32 = 0.81649658; // sqrt(2/3)
-const SQRT_3_FRAC_2: f32 = 0.8660254; // sqrt(3)/2
+pub fn clarke() -> Matrix3<FixedI16F16> {
+    let sqrt_frac_2_3: FixedI16F16 = FixedI16F16::from_num(0.81649658); // sqrt(2/3)
+    let sqrt_3_frac_2 = FixedI16F16::from_num(0.8660254); // sqrt(3)/2
+    let frac_1_sqrt_2: FixedI16F16 = FixedI16F16::from_num(core::f32::consts::FRAC_1_SQRT_2); // 1/sqrt(2)
 
-pub fn clarke() -> Matrix3<f32> {
     #[rustfmt::skip]
     let mat = Matrix3::new(
-        1., -0.5, -0.5,
-        0., SQRT_3_FRAC_2, 0.,
-        FRAC_1_SQRT_2, FRAC_1_SQRT_2, FRAC_1_SQRT_2
+        FixedI16F16::from_num(1), FixedI16F16::from_num(-0.5), FixedI16F16::from_num(-0.5),
+        FixedI16F16::from_num(0), sqrt_3_frac_2, FixedI16F16::from_num(0),
+        frac_1_sqrt_2, frac_1_sqrt_2, frac_1_sqrt_2
     );
 
-    SQRT_FRAC_2_3 * mat
+    mat * sqrt_frac_2_3
 }
+
+// pub fn park(theta: f32) -> Matrix3<f32> {
+//     #[rustfmt::skip]
+//     let mat = Matrix3::new(
+//         theta.cos(), -0.5, -0.5,
+//         0., SQRT_3_FRAC_2, 0.,
+//         FRAC_1_SQRT_2, FRAC_1_SQRT_2, FRAC_1_SQRT_2
+//     );
+
+//     mat
+// }
