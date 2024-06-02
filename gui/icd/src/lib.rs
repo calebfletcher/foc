a7ae56d805d977097b73c0f5820d3f5ee9fc5b35
Caleb Fletcher <caleb@fletcher.cf>
2024-06-02 22:49:25 +1000

    feat(gui): add icd for comms protocol

--- gui/icd/src/lib.rs
+++ gui/icd/src/lib.rs
@@ -1,1 +1,16 @@
+#![no_std]
 
+use postcard::experimental::schema::Schema;
+use postcard_rpc::endpoint;
+use serde::{Deserialize, Serialize};
+
+endpoint!(PingEndpoint, u32, u32, "ping");
+
+endpoint!(WriteValueEndpoint, u32, (), "value/write");
+endpoint!(ReadValueEndpoint, (), State, "value/read");
+
+#[derive(Debug, PartialEq, Serialize, Deserialize, Schema)]
+pub struct State {
+    pub value: u32,
+    pub times_written: u32,
+}
